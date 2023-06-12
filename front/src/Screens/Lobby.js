77a224b3a439f344dc51ba643ea07843f5c3672d
tylers122 <tylersee99@gmail.com>
2023-06-12 02:17:17 -0700

    styled lobby

--- front/src/Screens/Lobby.js
+++ front/src/Screens/Lobby.js
@@ -190,52 +190,37 @@ class Lobby extends react.Component{
                 key={this.state.selectedForm}/>;
             } 
         }
-        else{
+        else {
             display = (
-                <div>
-                  <Button onClick={() => this.setState({ showForm: true, selectedForm: "createRoom" })}>
-                    Create a room
-                  </Button>
-                  <Button onClick={() => this.setState({ showForm: true, selectedForm: "joinRoom" })}>
-                    Join a room
-                  </Button>
-                  <Button onClick={() => this.setState({ showForm: true, selectedForm: "deleteRoom" })}>
-                    Delete a room
-                  </Button>
-                </div>
-              );
-        }
-
-        this.socket.on('update room after deletion', (data)=>{
-            fetch(this.props.server_url + '/api/rooms/all', {
-                method: "GET",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json",
-                }
-            }).then((res) => {
-                res.json().then((data) => {
-                    this.state.rooms = data;
-                    this.setState({rooms:data})
-                })
-            });
-        })
-
-        return(
+              <div className="button-container">
+                <Button onClick={() => this.setState({ showForm: true, selectedForm: "createRoom" })}>
+                  Create a room
+                </Button>
+                <Button onClick={() => this.setState({ showForm: true, selectedForm: "joinRoom" })}>
+                  Join a room
+                </Button>
+                <Button onClick={() => this.setState({ showForm: true, selectedForm: "deleteRoom" })}>
+                  Delete a room
+                </Button>
+              </div>
+            );
+          }
+        
+          this.socket.on("update room after deletion", (data) => {
+            // Update room after deletion logic
+          });
+        
+          return (
             <div className="lobby">
-                {this.state.screen === "starting" ? <h1>Lobby</h1> : <div> Room: {this.state.room} </div>}
-                {this.state.screen === "starting" ? (
-                    <StartingPage rooms={this.state.rooms} roomSelect={this.roomSelect} />
-                ) : (
-                    <Chatroom username={this.state.username} sendChat={this.sendChat} socket={this.socket} goBack={this.goBack} />
-                )}
-                {this.state.screen === "starting" ? (
-                    display
-                ) : (
-                <div></div>
-                )}
+              {this.state.screen === "starting" ? <h1>Lobby</h1> : <div className="room-info">Room: {this.state.room}</div>}
+              {this.state.screen === "starting" ? (
+                <StartingPage rooms={this.state.rooms} roomSelect={this.roomSelect} />
+              ) : (
+                <Chatroom username={this.state.username} sendChat={this.sendChat} socket={this.socket} goBack={this.goBack} />
+              )}
+              {this.state.screen === "starting" ? display : <div className="empty-container"></div>}
             </div>
-        );
+          );
     }
 }
 
